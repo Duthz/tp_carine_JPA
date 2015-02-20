1617856a8cf1d0fb9e881f78cdae1b788620dce5
Duthz <pierre.gaillard89140@gmail.com>
2015-02-20 10:05:52 +0100

    Primary push
    Salut

---
+++ ex-Struts-JPA/build/generated-sources/ap-source-output/com/myapp/model/Cotations_.java
@@ -0,0 +1,15 @@
+package com.myapp.model;
+
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-26T20:44:32")
+@StaticMetamodel(Cotations.class)
+public class Cotations_ { 
+
+    public static volatile SingularAttribute<Cotations, String> symbole;
+    public static volatile SingularAttribute<Cotations, BigDecimal> cours;
+
+}
